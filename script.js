478bf7e3c69085e1b742aa9b224b5d70fbbdaec6
Amir3an <amiralisayad0019@gmail.com>
2024-10-07 22:30:25 +0330

    change

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
+const menu = document.querySelector('nav');
 const openMenu = document.querySelector('.burger-icon');
 
 
 
 openMenu.addEventListener(onclick , () => {
-    if()
+    if(menu.w)
 })
