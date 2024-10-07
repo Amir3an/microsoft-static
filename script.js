a8d22ca03b42edf18a485572d9f630d28b7be29a
Amir3an <amiralisayad0019@gmail.com>
2024-10-07 22:29:27 +0330

    add Js

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-// const FirstMenu = document.querySelector(".FirstMenu");
-// const ul = document.querySelector(".ul");
-// const pay = document.querySelector(".pay");
+
+const openMenu = document.querySelector('.burger-icon');
 
 
-// function showModal_function(){
-//     ul.style.display="block";
-//     // container.style.background = "red"
-//     // container.style.opacity = "0.4"
-//     // backdrop.style.display="block";
-// }
 
-
-// function closeModal_function(){
-//     ul.style.display="none";
-//     // backdrop.style.display="none";
-// }
-
-// FirstMenu.addEventListener("click" ,showModal_function);
-// pay.addEventListener("click",closeModal_function);
+openMenu.addEventListener(onclick , () => {
+    if()
+})
