cee513ddd2c4ea7b2b30facf1a59d10c2f937171
Amir3an <amiralisayad0019@gmail.com>
2024-10-07 21:39:23 +0330

    im init

---
+++ script.js
@@ -0,0 +1,20 @@
+// const FirstMenu = document.querySelector(".FirstMenu");
+// const ul = document.querySelector(".ul");
+// const pay = document.querySelector(".pay");
+
+
+// function showModal_function(){
+//     ul.style.display="block";
+//     // container.style.background = "red"
+//     // container.style.opacity = "0.4"
+//     // backdrop.style.display="block";
+// }
+
+
+// function closeModal_function(){
+//     ul.style.display="none";
+//     // backdrop.style.display="none";
+// }
+
+// FirstMenu.addEventListener("click" ,showModal_function);
+// pay.addEventListener("click",closeModal_function);
