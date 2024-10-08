6b60564e2859e0511ccbb4c1fd6807a549c8283c
Amir3an <amiralisayad0019@gmail.com>
2024-10-08 20:34:24 +0330

    change

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menu = document.querySelector("ul");
   const openMenu = document.querySelector(".burger-icon");
-  const checkBox = document.querySelector(".line");
 
   openMenu.addEventListener("click", () => {
-    if (checkBox == "checked") {
+    if (menu.style.right == "-105%") {
       menu.style.right = "5%";
     } else {
       menu.style.right = "-105%";
