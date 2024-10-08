2ae99ef10c71f64cc23d23d0bdf2cd6bdd59491d
Amir3an <amiralisayad0019@gmail.com>
2024-10-08 20:28:53 +0330

    change

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const menu = document.querySelector(".ul");
-  const openMenu = document.querySelector(".burger-icon");
+  const menu = document.querySelector("ul");
+  const openMenu = document.querySelector(".onclick");
 
-  openMenu.addEventListener("checked", () => {
+  openMenu.addEventListener("click", () => {
     if (menu.style.right == "-105%") {
-      menu.style.right == "5%";
+      menu.style.right = "5%";
     } else {
-      menu.style.right == "-105%";
+      menu.style.right = "-105%";
     }
   });
 });
